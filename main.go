a26fd653981e1b0c4005fa66500107ca9bbb00df
Tim <timothy.j.lawless@outlook.com>
2023-10-09 13:17:40 -0400

    lesson complete

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+// Declare a Animal interface
 type Animal interface {
 	Says() string
 	NumberOfLegs() int
 }
 
+// Create 2 different structs
 type Dog struct {
 	Name  string
 	Breed string
@@ -17,11 +19,13 @@ type Gorilla struct {
 	NumberOfTeeth int
 }
 
+// Create a function that will print some output when we run the program.
 func PrintInfo(a Animal) {
 	fmt.Println("This animal says ", a.Says(), "and has ",
 		a.NumberOfLegs(), " legs.")
 }
 
+// Use the * to denote pointers to the d varriable
 func (d *Dog) Says() string {
 	return "Woof!"
 }
@@ -29,6 +33,7 @@ func (d *Dog) NumberOfLegs() int {
 	return 4
 }
 
+// Use the * to denote pointers to the d varriable
 func (d *Gorilla) Says() string {
 	return "Eee Eee, Ooh Ooh!"
 }
@@ -36,12 +41,15 @@ func (d *Gorilla) NumberOfLegs() int {
 	return 2
 }
 func main() {
+
+	// Create a new dog variable using or dog Struct
+	// Call the PrintInfo Function
 	dog := Dog{
 		Name:  "Richard",
 		Breed: "German Shepard",
 	}
 	PrintInfo(&dog)
-
+	// Create a new dog variable using or dog Struct
 	gorilla := Gorilla{
 		Name:          "Jock",
 		Color:         "grey",
